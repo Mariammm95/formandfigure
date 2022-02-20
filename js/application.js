71e8337bed6ddd95b707f00f87d4b6ca178e45af
Mariammm95 <mariammajid95@gmail.com>
2022-02-20 17:19:04 +0300

    commit 3

---
+++ js/application.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	"use strict";
+	
+	
+	$('#').on('click', function(){
+		  
+		$('html, body').animate({scrollTop: 0}, 1000);
+	
+	});
+		
+		 
+	
+	
+});
+	
